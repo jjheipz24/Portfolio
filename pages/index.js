e3e8888aa3a679e28d1a669f0a267227f26ba3ab
Jin Jin Heipler <heipler.jj@gmail.com>
2021-01-12 23:54:54 -0500

    Added link to existing site

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
       <div className={styles.container}>
         <h1>Hi, I'm Jin Jin</h1>
         <p>My site is currently under construction. Check back soon.</p>
+        <br />
+        <p className={styles.siteLink}>My pre-existing portfolio site can be found <a href="https://jjheipz24.github.io" title="Jin Jin's Portfolio Site">here</a></p>
       </div>
     </Layout>
   )

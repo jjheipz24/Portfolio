8a2a34ba0e2377bb5e83b0ce76c1a7a7b9cc197c
Jin Jin Heipler <heipler.jj@gmail.com>
2021-01-16 01:44:07 -0500

    Completed responsive nav

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/layout.module.scss'
-import React, { useState } from 'react'
+import React from 'react'
 import NavBar from './navbar.js'
 
-var classNames = require('classnames');
-
 export default function Layout({ children }) {
-    let toggle = false;
-
-    const [toggleClassName, setToggleClassName] = useState(styles.navbarLinks);
-
-    const handleClassChange = () => {
-        console.log("clicked")
-        toggleClassName === styles.navbarLinks ? setToggleClassName("hidden") : styles.navbarLinks;
-    };
 
     return (
         <div>
@@ -21,28 +11,8 @@ export default function Layout({ children }) {
                 <title>Jin Jin Heipler</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             </Head>
+            <NavBar></NavBar>
             <main>
-                {/* <nav className={styles.navbar}>
-                    <span></span>
-                    <i className="fas fa-bars fa-2x"></i>
-                    <div className={toggleClassName} onClick={handleClassChange}>
-                        <ul>
-                            <li>
-                                <Link href="/"><a>Home</a></Link>
-                            </li>
-                            <li>
-                                <Link href="/projects"><a>Projects</a></Link>
-                            </li>
-                            <li>
-                                <Link href="/about"><a>About</a></Link>
-                            </li>
-                            <li>
-                                <Link href="/resume"><a>Resume</a></Link>
-                            </li>
-                        </ul>
-                    </div>
-                </nav> */}
-                <NavBar></NavBar>
                 <div className={styles.container}>
                     <div className={styles.children}>
                         {children}
--- components/navbar.js
+++ components/navbar.js
@@ -3,34 +3,34 @@ import { Nav } from 'react-bootstrap'
 import styles from '../styles/navbar.module.scss'
 import Link from 'next/link'
 
-export default function NavBar(){
-    return(
-        <Navbar>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="justify-content-end ml-auto">
-                <Nav.Item>
-                    <Link href="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Link href="/projects">
-                        <Nav.Link>Projects</Nav.Link>
-                    </Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Link href="/about">
-                        <Nav.Link>About</Nav.Link>
-                    </Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Link href="/">
-                        <Nav.Link>Resume</Nav.Link>
-                    </Link>
-                </Nav.Item>
-            </Nav>
-        </Navbar.Collapse>
-    </Navbar>
+export default function NavBar() {
+    return (
+        <Navbar className={`navbar-dark ${styles.navbar}`} expand="lg">
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" className={styles.hamburger} />
+            <Navbar.Collapse id="responsive-navbar-nav" className={styles.collapsedBar}>
+                <Nav className="justify-content-end ml-auto">
+                    <Nav.Item className={styles.item}>
+                        <Link href="/">
+                            <a className={styles.linkItem}>Home</a>
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item className={styles.item}>
+                        <Link href="/projects">
+                            <a className={styles.linkItem}>Projects</a>
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item className={styles.item}>
+                        <Link href="/about">
+                            <a className={styles.linkItem}>About</a>
+                        </Link>
+                    </Nav.Item>
+                    <Nav.Item className={styles.item}>
+                        <Link href="/">
+                            <a className={styles.linkItem}>Resume</a>
+                        </Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }

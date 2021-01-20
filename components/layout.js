236f93e19288a49902a6c13a362a9616d467c8c8
Jin Jin Heipler <heipler.jj@gmail.com>
2021-01-20 14:21:15 -0500

    Changed how vid background is made. Added styles to home page

--- components/layout.js
+++ components/layout.js
@@ -4,32 +4,28 @@ import React from 'react'
 import NavBar from './navbar.js'
 import SocialMedia from './socialmedia.js'
 
-export default function Layout({ children }) {
+export default function Layout({ children, type }) {
     return (
         <div>
             <Head>
                 <title>Jin Jin Heipler</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             </Head>
-            <NavBar></NavBar>
-            <main>
-                <div className={styles.container}>
-                    <div className={styles.children}>
-                        {children}
-                        <footer className={styles.footer}>
-                            <SocialMedia></SocialMedia>
-                            <p>&copy; 2021 Jin Jin Heipler</p>
-                        </footer>
-                    </div>
-                    <div className={styles.vidContainer}>
-                        <video playsInline autoPlay loop muted poster="/images/space-background.png" className={styles.bgVideo}>
-                            <source src="/videos/space-background.mp4" type="video/mp4" />
-                            <source src="/videos/space-background.wmv" type="video/wmv" />
-                            <source src="/videos/space-background.webm" type="video/webm" />
-                            <p>Your browser does not support this video.</p>
-                        </video>
-                    </div>
-                </div>
+            <NavBar type={type}></NavBar>
+            <div className={styles.vidContainer}>
+                <video playsInline autoPlay loop muted poster="/images/space-background.png" className={styles.bgVideo}>
+                    <source src="/videos/space-background.mp4" type="video/mp4" />
+                    <source src="/videos/space-background.wmv" type="video/wmv" />
+                    <source src="/videos/space-background.webm" type="video/webm" />
+                    <p>Your browser does not support this video.</p>
+                </video>
+            </div>
+            <main className={styles.children}>
+                {children}
+                <footer className={styles.footer}>
+                    <SocialMedia></SocialMedia>
+                    <p>&copy; 2021 Jin Jin Heipler</p>
+                </footer>
             </main>
         </div>
     )
